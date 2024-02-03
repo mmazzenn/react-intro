f1705adf0048130991aa6e739e4fca3699ba5035
mmazzenn <ma6111696@gmail.com>
2024-02-03 21:05:47 +0200

    Deploy 3

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ function App() {
   let routes = createBrowserRouter([
     {path: "", element: <Layout/>, children: [
       {index: true, element: <Home/>},
+      {path: "", element: <Home/>},
       {path: "home", element: <Home/>},
       {path: "about", element: <About/>},
       {path: "portfolio", element: <Portfolio/>},
